9558357d845d84bcc7211d65616482425ec4a1c9
PabloMartinTrujillo <72193242+PabloMartinTrujillo@users.noreply.github.com>
2020-10-14 16:39:36 +0200

    Add files via upload

---
+++ Tema1/MiNombreMejorado.java
@@ -0,0 +1,7 @@
+public class MiNombreMejorado {
+  public static void main (String[] args) {
+    System.out.println("Mi nombre es Pablo");
+    System.out.println("Direccion: Pepita 25");
+    System.out.print("Numero de telefono: 473885638");
+  }
+}
